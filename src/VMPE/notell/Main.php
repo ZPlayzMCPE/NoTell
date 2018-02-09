175ae229a1538cda5ee921c6ae771578d47d8fa0
iZeaoGamer <jameshayward14@icloud.com>
2018-02-09 20:50:23 +0000

    Added files.

---
+++ src/VMPE/notell/Main.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace NoTell\notell;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+class Main extends PluginBase implements Listener {
+
+    private $enabled;
+
+    public function onEnable() {
+        $this->enabled = [];
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $issuer, Command $cmd, string $label, array $args) : bool{
+        if ((strtolower($cmd->getName()) == "notell") && !(isset($args[0])) && ($issuer instanceof Player) && ($issuer->hasPermission("notell.toggle") || $issuer->hasPermission("notell.toggle.self"))) {
+            if (isset($this->enabled[strtolower($issuer->getName())])) {
+                unset($this->enabled[strtolower($issuer->getName())]);
+            } else {
+                $this->enabled[strtolower($issuer->getName())] = strtolower($issuer->getName());
+            }
+
+            if (isset($this->enabled[strtolower($issuer->getName())])) {
+                $issuer->sendMessage("NoTell mode enabled!");
+            } else {
+                $issuer->sendMessage("NoTell mode disabled!");
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function onPlayerCommand(PlayerCommandPreprocessEvent $event) {
+        if ($event->isCancelled()) return;
+        $message = $event->getMessage();
+        if (strtolower(substr($message, 0, 3)) == "/m" || strtolower(substr($message, 0, 5)) == "/tell" || strtolower(substr($message, 0, 7)) == "/msg" || strtolower(substr($message, 0, 4)) == "/t") {
+            $args = explode(" ", $message);
+            if (!isset($args[1])) {
+                return;    
+            }
+            $sender = $event->getPlayer();
+
+            foreach ($this->enabled as $notelluser) {
+
+                if ((strpos(strtolower($notelluser), strtolower($args[1])) !== false) && (strtolower($notelluser) !== strtolower($sender->getName()))) {
+                    $sender->sendMessage(TextFormat::RED . "§5This Player Is Not Accepting private messages");
+                    $event->setCancelled(true);
+                    return;
+                }
+
+                if (isset($args[2]) && strpos(strtolower($notelluser), strtolower($args[2])) !== false && (strtolower($notelluser) !== strtolower($sender->getName()))) {
+                    $sender->sendMessage(TextFormat::RED . "§5This Player Is Not Accepting private messages");
+                    $event->setCancelled(true);
+                    return;
+                }
+            }
+        }
+    }
+}
